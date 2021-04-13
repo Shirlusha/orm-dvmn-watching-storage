4ffb2daa3f773d0e0ce7db25a3be9b3bb0993fb1
Shirlusha <sheerly.morra@gmail.com>
2021-04-13 22:29:44 +0300

    Add project V1 include README.md and requirements.txt

---
+++ project/settings.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+from environs import Env
+
+env = Env()
+
+load_dotenv()
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'HOST': env("HOST"),
+        'PORT': env("PORT"),
+        'NAME': env("NAME"),
+        'USER': env("USER"),
+        'PASSWORD': env("PASSWORD")
+    }
+}
+
+INSTALLED_APPS = ["datacenter"]
+
+SECRET_KEY = env("SECRET_KEY")
+
+DEBUG = env.bool("DEBUG", False)
+
+ROOT_URLCONF = "project.urls"
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+    },
+]
+
+
+USE_L10N = True
+
+LANGUAGE_CODE = "ru-ru"
+
+TIME_ZONE = "Europe/Moscow"
+
+USE_TZ = True
